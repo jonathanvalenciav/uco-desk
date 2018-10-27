0c2f2758bae8034ab26439da85ec3318d73203ca
Jonathan Alexis Valencia <alexisvalenciav@gmail.com>
2018-10-27 15:31:38 -0500

    Initial commit

---
+++ src/test/java/com/edu/uco/ucodesk/UcodeskApplicationTests.java
@@ -0,0 +1,16 @@
+package com.edu.uco.ucodesk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UcodeskApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
